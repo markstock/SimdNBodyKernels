b7bb7ace0591716958ec2f63b0e0e707b3d4a6fc
Mark Stock <markjstock@gmail.com>
2015-02-11 20:04:03 -0700

    Added working grav2Daccel

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # SimdNBodyKernels
 Easy-to-implement n-body simulation kernels created using Intel's ispc and llvm/clang
+
+
+This package contains several simple single-step programs for force summation 
+using the direct O(N^2) n-body method, accelerated with both SIMD extensions
+(SSE2, AVX) and multithreading using Intel's ispc compiler. These blocks of code
+should be easy to incorporate into your own n-body simulation projects.
+
+
---
+++ gravitation2Daccel/nbody_serial.cpp
@@ -0,0 +1,31 @@
+/*
+  Copyright (c) 2015, Mark J Stock
+*/
+
+
+static inline void nbody_kernel_serial(float sx, float sy, float ss,
+                                       float tx, float ty,
+                                       float* tax, float* tay) {
+    // 10 flops
+    float dx = sx - tx;
+    float dy = sy - ty;
+    float r2 = dx*dx + dy*dy;
+    r2 = ss/r2;
+    *tax += r2 * dx;
+    *tay += r2 * dy;
+}
+
+void nbody_serial(int numSrcs, float sx[], float sy[], float ss[],
+                  int numTarg, float tx[], float ty[], float ts[],
+                  float tax[], float tay[])
+{
+    for (int i = 0; i < numTarg; i++) {
+        tax[i] = 0.0;
+        tay[i] = 0.0;
+        for (int j = 0; j < numSrcs; j++) {
+            nbody_kernel_serial(sx[j], sy[j], ss[j], tx[i], ty[i], &tax[i], &tay[i]);
+        }
+        tax[i] /= ts[i];
+        tay[i] /= ts[i];
+    }
+}
